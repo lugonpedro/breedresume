93fb28f4565f35faa24cf017325dd4e1beb35473
Pedro Lugon <pedromotta20@gmail.com>
2024-08-28 14:45:35 -0300

    feat(login): change layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,9 +3,14 @@ import { LoginForm } from "./login-form";
 
 export default async function Login() {
   return (
-    <>
-      <LoginForm />
-      <Link href="/register">Criar Conta</Link>
-    </>
+    <div className="bg-slate-100 text-black min-h-screen">
+      <div className="p-4 max-w-[800px] mx-auto flex flex-col justify-center h-screen">
+        <p className="text-2xl text-center font-semibold">Login</p>
+        <LoginForm />
+        <Link href="/register" className="mt-2 underline text-end">
+          Criar Conta
+        </Link>
+      </div>
+    </div>
   );
 }
