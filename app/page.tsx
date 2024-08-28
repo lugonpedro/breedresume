7cdcb9ad6ea7b1feb4acae525298a7ec8a525723
Pedro Lugon <pedromotta20@gmail.com>
2024-08-28 14:46:54 -0300

    feat(home): change layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  // const { data, error } = await supabase.from("countries").select();
-
-  // console.log(data, error);
-
-  return <></>;
+  return (
+    <>
+      <div className="bg-slate-100 text-black min-h-screen">
+        <div className="px-4 pt-20 pb-8 max-w-[1300px] mx-auto flex flex-col gap-20">
+          <div className="text-center">
+            <h1 className="text-5xl font-semibold">BreedResume</h1>
+            <p className="text-black/50">
+              Ser um generalista é bom a menos que você queira encontrar uma
+              vaga no mercado de trabalho
+            </p>
+            <Link href="/login">
+              <Button className="mt-10 uppercase font-normal">
+                Começar agora
+              </Button>
+            </Link>
+          </div>
+          <div>
+            <h2 className="text-2xl">
+              Você não precisa mais reescrever todo o seu currículo quando se
+              interessar por uma vaga.
+            </h2>
+            <p>Veja como é fácil de usar o BreedResume:</p>
+            <p>Imagens/Gifs</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
