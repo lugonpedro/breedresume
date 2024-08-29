37da798e2977a3ad895e6b64e7541a0133173cce
Pedro Lugon <pedromotta20@gmail.com>
2024-08-28 22:39:17 -0300

    feat(auth): change to zustand subscribe

--- src/pages/panel/navbar.tsx
+++ src/pages/panel/navbar.tsx
@@ -10,37 +10,42 @@ import { supabase } from "@/services/supabase/client";
 import { logout } from "@/services/supabase/log-out";
 import { User as UserSupabase } from "@supabase/supabase-js";
 import { AnimatePresence, motion } from "framer-motion";
-import {
-  BrickWall,
-  ChevronDown,
-  Cuboid,
-  LayoutDashboard,
-  Menu,
-  User,
-  X,
-} from "lucide-react";
+import { BrickWall, ChevronDown, Cuboid, Menu, User, X } from "lucide-react";
 import { ReactNode, useEffect, useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const [user, setUser] = useState<UserSupabase | null>(null);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function getUser() {
       const { data, error } = await supabase.auth.getUser();
 
       if (error || !data?.user) {
-        // router.replace("/login");
+        navigate("/login");
       }
 
       authContext.setState({
         user: { ...data.user! },
       });
-      setUser({ ...data.user! });
     }
 
     getUser();
   }, []);
+
+  useEffect(() => {
+    const unsub = authContext.subscribe((state, prevState) => {
+      if (state.user === null) {
+        navigate("/login");
+      }
+
+      setUser(state.user);
+    });
+
+    return unsub;
+  }, [navigate]);
 
   return (
     <>
@@ -50,8 +55,8 @@ export default function Navbar() {
       <div className="hidden xl:block">
         <NavbarDesktop user={user} />
       </div>
-      <div className="bg-slate-800 min-h-screen">
-        <main className="primaryContainer text-white">
+      <div className="bg-primary min-h-screen">
+        <main className="primaryContainer">
           <Outlet />
         </main>
       </div>
@@ -63,30 +68,15 @@ function NavbarLinks() {
   return (
     <>
       <NavbarItem
-        icon={<LayoutDashboard size={14} />}
-        title="Painel"
-        link="/dashboard"
+        icon={<BrickWall size={14} />}
+        title="Experiências"
+        link="/experiences"
       />
       <NavbarItem
         icon={<Cuboid size={14} />}
         title="Habilidades"
         link="/skills"
       />
-      <NavbarItem
-        icon={<BrickWall size={14} />}
-        title="Experiências"
-        link="/experiences"
-      />
-      {/* <NavbarDropdownItem
-        icon={<HeartPulse size={14} />}
-        title="Testando"
-        subItems={[
-          {
-            title: "Teste",
-            link: "/teste",
-          },
-        ]}
-      /> */}
     </>
   );
 }
@@ -101,11 +91,11 @@ function ProfileButton({ user }: { user: UserSupabase | null }) {
       )}
       <DropdownMenuContent
         side="right"
-        className="p-2 bg-slate-200 rounded-t-none text-black hidden md:block"
+        className="p-2 bg-secondary text-primary hidden md:block"
       >
-        <Link to="/profile">
+        {/* <Link to="/profile">
           <DropdownMenuItem>Perfil</DropdownMenuItem>
-        </Link>
+        </Link> */}
         <DropdownMenuItem onClick={logout} className="cursor-pointer">
           Sair
         </DropdownMenuItem>
@@ -116,10 +106,10 @@ function ProfileButton({ user }: { user: UserSupabase | null }) {
 
 function NavbarDesktop({ user }: { user: UserSupabase | null }) {
   return (
-    <nav className="fixed z-50 flex h-screen w-56 flex-col justify-between bg-slate-200 py-5 border-r">
+    <nav className="fixed z-50 flex h-screen w-56 flex-col justify-between bg-secondary py-5 border-r">
       <div>
         <div className="leading-none text-center mb-4">
-          <p className="text-xl">BreedResume</p>
+          <p className="text-xl text-primary font-semibold">BreedResume</p>
         </div>
         <NavbarLinks />
       </div>
@@ -132,16 +122,16 @@ function NavbarMobile({ user }: { user: UserSupabase | null }) {
   const [opened, setOpened] = useState<boolean>(false);
 
   return (
-    <header className="bg-slate-200 fixed top-0 left-0 w-full h-16 flex items-center justify-between px-4 z-50 border-b shrink-0">
+    <header className="bg-secondary absolute top-0 left-0 w-full h-16 flex items-center justify-between px-4 z-50 border-b shrink-0">
       <div className="flex flex-row items-center">
         <div className="leading-none">
-          <p className="text-xl font-semibold">BR</p>
+          <p className="text-xl font-semibold text-primary">BR</p>
         </div>
       </div>
       {opened ? (
-        <X onClick={() => setOpened(false)} className="stroke-black" />
+        <X onClick={() => setOpened(false)} className="stroke-primary" />
       ) : (
-        <Menu onClick={() => setOpened(true)} className="stroke-black" />
+        <Menu onClick={() => setOpened(true)} className="stroke-primary" />
       )}
       <AnimatePresence>
         {opened && (
@@ -150,7 +140,7 @@ function NavbarMobile({ user }: { user: UserSupabase | null }) {
             animate={{ y: 0, opacity: 1 }}
             exit={{ y: -300, opacity: 0 }}
             transition={{ duration: 0.5 }}
-            className={`bg-slate-200 fixed py-4 px-6 flex flex-col items-start gap-4 left-0 top-14 rounded-b-lg z-50 w-full ${
+            className={`bg-secondary absolute py-4 px-6 flex flex-col items-start gap-4 left-0 top-14 rounded-b-lg z-50 w-full ${
               opened ? "pointer-events-auto" : "pointer-events-none"
             }`}
           >
@@ -184,25 +174,25 @@ function NavbarItem(props: NavbarItemProps) {
       {props.link && (
         <Link
           to={props.link}
-          className={`flex w-full items-center px-3 py-2 text-black duration-300 hover:bg-mainBlue/30 ${
+          className={`flex w-full items-center px-3 py-2 text-primary duration-300 hover:bg-mainBlue/30 ${
             actualPage === props.link || actualPage === props.link
               ? "bg-mainBlue/30 hover:bg-mainBlue/30"
               : ""
           }`}
         >
           {props.icon}
-          <p className={`text-sm text-black ${props.icon ? "ml-2" : ""}`}>
+          <p className={`text-sm text-primary ${props.icon ? "ml-2" : ""}`}>
             {props.title}
           </p>
         </Link>
       )}
       {!props.link && (
         <div
-          className="flex w-full cursor-pointer items-center px-3 py-2 text-black duration-300 hover:bg-mainBlue/30"
+          className="flex w-full cursor-pointer items-center px-3 py-2 text-primary duration-300 hover:bg-mainBlue/30"
           onClick={props.onClick}
         >
           {props.icon}
-          <p className={`text-sm text-black ${props.icon ? "ml-2" : ""}`}>
+          <p className={`text-sm text-primary ${props.icon ? "ml-2" : ""}`}>
             {props.title}
           </p>
         </div>
@@ -289,7 +279,7 @@ function NavbarDropdownItem(props: NavbarDropdownItemProps) {
     <>
       <button
         onClick={() => setOpened(!opened)}
-        className="flex w-full flex-row items-center justify-between gap-2 px-3 py-2 text-black duration-300 hover:bg-mainBlue/30"
+        className="flex w-full flex-row items-center justify-between gap-2 px-3 py-2 text-primary duration-300 hover:bg-mainBlue/30"
       >
         <div className="flex items-center gap-2">
           {props.icon}
@@ -320,18 +310,18 @@ function NavbarDropdownItem(props: NavbarDropdownItemProps) {
                 {subItem.link && (
                   <Link
                     to={subItem.link}
-                    className={`flex w-full items-center px-8 py-2 text-black duration-300 hover:bg-mainBlue/30 ${
+                    className={`flex w-full items-center px-8 py-2 text-primary duration-300 hover:bg-mainBlue/30 ${
                       actualPage === subItem.link || actualPage === subItem.link
                         ? "bg-mainBlue/30 hover:bg-mainBlue/30"
                         : ""
                     }`}
                   >
-                    <p className="text-sm text-black">{subItem.title}</p>
+                    <p className="text-sm text-primary">{subItem.title}</p>
                   </Link>
                 )}
                 {!subItem.link && (
                   <div
-                    className="flex w-full px-4 py-2 text-black duration-300 hover:bg-mainBlue/30"
+                    className="flex w-full px-4 py-2 text-primary duration-300 hover:bg-mainBlue/30"
                     onClick={subItem.onClick}
                   >
                     {subItem.title}
